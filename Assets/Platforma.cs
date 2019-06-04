33a607bc5a03959b627dea1e46df94d15d75a230
AgnieszkaKowalewska <agnieszka.bb.kowalewska@gmail.com>
2019-06-04 23:26:08 +0200

    Commit :)

---
+++ Assets/Platforma.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforma : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 PlatformaPos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = PlatformaPos;
+    }
+}
